9887c41bd295b7ff9797fe6a08757aec486ffb71
bbscoding <batuhan5534@gmail.com>
2023-10-09 22:13:04 +0300

    api ve navbar

---
+++ src/Requests.js
@@ -0,0 +1,10 @@
+const key = "c143f52ce813af340d60e01097fddbfc"
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/discover/movie?api_key=${key}&with_genres=27`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+
+}
