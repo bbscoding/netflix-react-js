53af840b180bbcbfd549f360c7d6d9050a050919
bbscoding <batuhan5534@gmail.com>
2023-10-23 19:18:16 +0300

    firebase setup and pages for auth

---
+++ src/pages/Signup.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Signup = () => {
+    return (
+        <>
+            <div className="w-full h-screen">
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/0ff5157d-6c35-4546-a844-5254b42bff8f/TR-tr-20231016-popsignuptwoweeks-perspective_alpha_website_small.jpg" />
+            </div>
+        </>
+    )
+}
+
+export default Signup;
